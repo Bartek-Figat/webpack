a25a29f2b21a8bb398654b8ca355cf74bafa8be1
Bartek-Figat <figat29@gmail.com>
2020-12-29 19:01:41 -0800

    test

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,4 +11,21 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html',
+      chunks: ['vanilla'],
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false,
+    }),
+  ],
 });
